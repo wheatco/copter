7915f77be0762102eb0afb5aa7eae40ba6cc36f8
Robert Lord <robert@lord.io>
2016-03-07 20:22:15 -0600

    Add some functions to easily control motors

--- firm/src/main.rs
+++ firm/src/main.rs
@@ -32,71 +32,81 @@ const INVRT: u8 = 0x10;
 const OUTDRV: u8 = 0x04;
 
 // const TARGET_ADDR: u16 = 0x68; // accelerometer
-const TARGET_ADDR: u16 = 0x40; // motor controllers
 
-fn setPWM(dev: &mut LinuxI2CDevice, chan: u8, on: u16, off: u16) {
-    dev.smbus_write_byte_data(LED0_ON_L+4*chan, (on & 0xFF) as u8);
-    dev.smbus_write_byte_data(LED0_ON_H+4*chan, (on >> 8) as u8);
-    dev.smbus_write_byte_data(LED0_OFF_L+4*chan, (off & 0xFF) as u8);
-    dev.smbus_write_byte_data(LED0_OFF_H+4*chan, (off >> 8) as u8);
+struct Motors {
+    device: LinuxI2CDevice,
 }
 
-// power must be 0 < n < 100
-fn setPower(dev: &mut LinuxI2CDevice, chan: u8, power: u16) {
-    if power <= 100 && power >= 0 {
-        // see http://distantorion.com/2014/10/24/motor-control/
-        // this makes output pulses between 1ms and 2ms
-        setPWM(dev, chan, 0, power * 8 + 810);
+const MOTORS_ADDR: u16 = 0x40;
+
+// see also
+// https://learn.adafruit.com/downloads/pdf/adafruit-16-channel-servo-driver-with-raspberry-pi.pdf
+// https://github.com/adafruit/Adafruit-Raspberry-Pi-Python-Code
+//       /blob/master/Adafruit_PWM_Servo_Driver/Adafruit_PWM_Servo_Driver.py
+// http://distantorion.com/2014/10/24/motor-control/
+impl Motors {
+    fn new(i2c_name: &'static str) -> Motors {
+        let mut motors = Motors {
+            device: LinuxI2CDevice::new(i2c_name, MOTORS_ADDR).unwrap()
+        };
+
+        thread::sleep_ms(100);
+        motors.power(0.0,0.0,0.0,0.0);
+
+        // reset PCA9685
+        motors.device.smbus_write_byte_data(MODE2, OUTDRV);
+        motors.device.smbus_write_byte_data(MODE1, ALLCALL);
+        thread::sleep_ms(5); // wait for oscillator
+
+        // turn off sleep mode
+        let mode1 = motors.device.smbus_read_byte_data(MODE1).unwrap() & !SLEEP;
+        motors.device.smbus_write_byte_data(MODE1, mode1);
+        thread::sleep_ms(5); // wait for oscillator
+
+        motors
+    }
+
+    pub fn calibrate(&mut self) {
+        self.power(1.0, 1.0, 1.0, 1.0);
+        println!("Get ready to plug in battery...");
+        thread::sleep_ms(2000);
+        println!("Now!");
+        thread::sleep_ms(4000);
+        self.power(0.0, 0.0, 0.0, 0.0);
+        thread::sleep_ms(5000);
+    }
+
+    pub fn power(&mut self, motor1: f64, motor2: f64, motor3: f64, motor4: f64) {
+        self.power_one(0, 0, (motor1 * 800.0) as u16 + 810);
+        self.power_one(1, 0, (motor2 * 800.0) as u16 + 810);
+        self.power_one(2, 0, (motor3 * 800.0) as u16 + 810);
+        self.power_one(3, 0, (motor4 * 800.0) as u16 + 810);
+    }
+
+    fn power_one(&mut self, chan: u8, on: u16, off: u16) {
+        self.device.smbus_write_byte_data(LED0_ON_L+4*chan, (on & 0xFF) as u8);
+        self.device.smbus_write_byte_data(LED0_ON_H+4*chan, (on >> 8) as u8);
+        self.device.smbus_write_byte_data(LED0_OFF_L+4*chan, (off & 0xFF) as u8);
+        self.device.smbus_write_byte_data(LED0_OFF_H+4*chan, (off >> 8) as u8);
     }
 }
 
 // real code should not use unwrap() so liberally
 fn main() {
-    let mut device = LinuxI2CDevice::new("/dev/i2c-1", TARGET_ADDR).unwrap();
+    let mut motors = Motors::new("/dev/i2c-1");
     thread::sleep_ms(100);
 
-    // reset PCA9685
-    setPWM(&mut device, 0, 0, 0);
-    setPWM(&mut device, 1, 0, 0);
-    setPWM(&mut device, 2, 0, 0);
-    setPWM(&mut device, 3, 0, 0);
-    device.smbus_write_byte_data(MODE2, OUTDRV);
-    device.smbus_write_byte_data(MODE1, ALLCALL);
-    thread::sleep_ms(5); // wait for oscillator
+    motors.calibrate();
 
-    // turn off sleep mode
-    let mode1 = device.smbus_read_byte_data(MODE1).unwrap() & !SLEEP;
-    device.smbus_write_byte_data(MODE1, mode1);
-    thread::sleep_ms(5); // wait for oscillator
-
-    let mut input = String::new();
-
-    // set max value and then wait for battery to be connected
-    setPower(&mut device, 0, 100);
-    setPower(&mut device, 1, 100);
-    setPower(&mut device, 2, 100);
-    setPower(&mut device, 3, 100);
-    println!("Plug in the battery now! Press enter when you hear the beep...");
-    io::stdin().read_line(&mut input);
-    thread::sleep_ms(3000); // wait 3 seconds after beep to drop pulse length
-    setPower(&mut device, 0, 0);
-    setPower(&mut device, 1, 0);
-    setPower(&mut device, 2, 0);
-    setPower(&mut device, 3, 0);
-
-    thread::sleep_ms(5000); // wait 5 seconds and then try powering the motor
-    setPower(&mut device, 0, 50);
+    motors.power(0.5, 0.0, 0.0, 0.0);
     thread::sleep_ms(5000);
-    setPower(&mut device, 0, 0);
-    setPower(&mut device, 1, 50);
+    motors.power(0.0, 0.5, 0.0, 0.0);
     thread::sleep_ms(5000);
-    setPower(&mut device, 1, 0);
-    setPower(&mut device, 2, 50);
+    motors.power(0.0, 0.0, 0.5, 0.0);
     thread::sleep_ms(5000);
-    setPower(&mut device, 2, 0);
-    setPower(&mut device, 3, 50);
+    motors.power(0.0, 0.0, 0.0, 0.5);
     thread::sleep_ms(5000);
-    setPower(&mut device, 3, 0);
+    motors.power(0.0, 0.0, 0.0, 0.0);
 
     // Now wake the 6050 up as it starts in sleep mode
     // device.smbus_write_byte_data(0x6b, 0);
