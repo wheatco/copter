6e8c8e9031eb6f7bdc10ab541b51d979c99c4643
Adam Canady <adam@adamcanady.com>
2016-04-12 22:50:42 -0500

    Add new settings file and modify build fc command to extract tar if it's not extracted yet

--- build_fc.sh
+++ build_fc.sh
@@ -1,5 +1,9 @@
 #!/bin/bash
-export PATH=$PATH:$(pwd)/gcc-arm-none-eabi-5_3-2016q1/bin
+if [ ! -d gcc-arm-none-eabi-4_9-2015q2/bin ]; then
+    tar -xzvf gcc-arm-none-eabi-4_9-2015q2-20150609-mac.tar.bz2
+fi;
+
+export PATH=$PATH:$(pwd)/gcc-arm-none-eabi-4_9-2015q2/bin
 echo $PATH
 cd fc
 make clean TARGET=NAZE
